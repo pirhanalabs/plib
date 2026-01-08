878e62e4e1fcac58e2d23e23f4e34dca199e0b1e
Christophe Bessette <christophe.bessette@hotmail.com>
2026-01-08 13:34:18 -0500

    [+] added MapList struct

---
+++ src/plib/common/structs/MapList.hx
@@ -0,0 +1,36 @@
+package plib.common.structs;
+
+abstract MapList<T, K>(Map<T, Array<K>>)
+{
+	public inline function new()
+	{
+		this = [];
+	}
+
+	public inline function add(key:T, value:K)
+	{
+		if (!hasKey(key))
+			createKey(key);
+		return this.get(key).push(value);
+	}
+
+	public inline function remove(key:T, value:K)
+	{
+		return hasKey(key) && this.get(key).remove(value);
+	}
+
+	public inline function has(key:T, value:K)
+	{
+		return hasKey(key) && this.get(key).contains(value);
+	}
+
+	public inline function hasKey(key:T)
+	{
+		return this.exists(key);
+	}
+
+	private inline function createKey(key:T)
+	{
+		this[key] = [];
+	}
+}
